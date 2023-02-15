c4c8530ae5e2a3b9e209e04c337cc490e5f9fd73
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-02-15 20:18:48 +0000

    got data to work!

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-params: // Variables
-const appid = 'ce02baa3a12a5fb92879d74991101fd8';
+//params: // Variables
+let appid = 'ce02baa3a12a5fb92879d74991101fd8';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 let zip = '';
 
-console.log(axios.get(url));
-let options = {
-  baseURL: BASE_URL,
-  params: {
-    zip: zip,
-    apiKey: appid,
-  }
-}
-axios.get('/everything', options)
+//console.log(axios.get(url));
+// let options = {
+//   baseURL: BASE_URL,
+//   params: {
+//     zip: zip,
+//     apiKey: appid,
+//   }
+// }
+axios.get(BASE_URL + '40356,us&appid=ce02baa3a12a5fb92879d74991101fd8')
   .then(function (response) {
     console.log(response);
   })

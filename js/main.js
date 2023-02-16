f3f26e80aeabce01658b65673d3f00da63939389
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-02-16 15:43:24 +0000

    added working data

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 //params: // Variables
 let appid = 'ce02baa3a12a5fb92879d74991101fd8';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+let bfApp = '&appid=';
+// hook zip up into a button to search for said zip code EX 40356 is nichvegas
 let zip = '';
-
-//console.log(axios.get(url));
+let data = []
+// console.log(axios.get(url));
 // let options = {
 //   baseURL: BASE_URL,
 //   params: {
@@ -11,9 +13,11 @@ let zip = '';
 //     apiKey: appid,
 //   }
 // }
-axios.get(BASE_URL + '40356,us&appid=ce02baa3a12a5fb92879d74991101fd8')
+axios.get(BASE_URL + '40356,us' + bfApp + appid)
   .then(function (response) {
     console.log(response);
+    data = response.data.results;
+    console.log(data);
   })
   .catch(function (error) {
     console.log(error);
@@ -21,3 +25,12 @@ axios.get(BASE_URL + '40356,us&appid=ce02baa3a12a5fb92879d74991101fd8')
   .then(function () {
     // always executed
   }); 
+  
+  //from morty api, check and see if u can use it to paste data. 
+  function createListItems(arr) {
+    for (const joke of arr) {
+        let listItem = document.createElement('li');
+        listItem.innerText = `${joke.data}: ${joke.temp}`;
+        document.getElementById('myList').appendChild(listItem);
+    }
+}

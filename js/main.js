a492864b5689e76fc91cd5dbb5a0b530123f9be0
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-02-17 00:40:49 +0000

    added finishing styles

--- js/main.js
+++ js/main.js
@@ -17,29 +17,36 @@ let Wthr = {
   let weatherHead = document.createElement ('h1')
   weatherHead.innerText = 'Weather App Project';
   weatherHead.classList = "text-center";
+  weatherHead.style.color = "white";
   weatherHead.id = "header1";
   main.appendChild(weatherHead);
 //add button AND input as well into same DIV
-
+  const btDiv = document.createElement ('div');
+  btDiv.style.textAlign = "center";
   const subButton = document.createElement ('button');
   subButton.innerText = 'Get Weather';
-  subButton.classList = "center";
   subButton.id = 'button-1';
   let input = document.createElement("input");
       input.id = 'zipCode';
       input.setAttribute("type", "text");
   subButton.appendChild(input);
   //Add empty box for typing zipcode````COMPLETED
-  main.appendChild(subButton);
+  main.appendChild(btDiv);
+  btDiv.appendChild(subButton);
   //Its own div with data for zip maybe? 
 let cityName = document.createElement ('div');
         cityName.id = "div1";
         cityName.classList = "text-center";
         cityName.style.border = "solid 2px"
+        cityName.style.color = "white";
+        cityName.style.marginTop = "15px";
+        cityName.style.fontSize = "25px";
+        cityName.style.height = "150px";
 
-let cityTxt = document.createTextNode ('City');
+let cityTxt = document.createTextNode ('City:');
 let cityData = document.createElement ('p');
 cityData.textContent = Wthr.city;
+cityData.style.fontSize = "40px";
   //leave empty for city data
 let citiTxt = document.createTextNode ('');
   //POSSIBLY NEED ID TO ADD DATA?
@@ -53,9 +60,14 @@ let Tempsec = document.createElement ('div');
             Tempsec.id = "div2";
             Tempsec.classList= "text-center";
             Tempsec.style.border = "solid 2px"
-let temptxt = document.createTextNode ('Temperature');
+            Tempsec.style.color = "white";
+            Tempsec.style.marginTop = "15px";
+            Tempsec.style.fontSize = "25px";
+            Tempsec.style.height = "150px";
+let temptxt = document.createTextNode ('Temperature:');
 let tempData = document.createElement ('p');
       tempData.textContent = Wthr.temperature;
+      tempData.style.fontSize = "40px";
 let tempTextdata = document.createTextNode ('');
   //Possibly add temp info here from id
           tempTextdata.id = "tempinfo";
@@ -68,9 +80,14 @@ let condSec = document.createElement ('div');
       condSec.id = "div3";
       condSec.classList = "text-center";
       condSec.style.border = "solid 2px"
-let condTxt = document.createTextNode ('Condition');
+      condSec.style.color = "white";
+      condSec.style.fontSize = "25px";
+      condSec.style.marginTop = "15px";
+      condSec.style.height = "150px";
+let condTxt = document.createTextNode ('Condition:');
 let condData = document.createElement ('p');
       condData.textContent = Wthr.conditions;
+      condData.style.fontSize = "40px";
 let condTextData = document.createTextNode ('');
       condTextData.id = "conddata";
       //practice plugging in to right slots, was hard
